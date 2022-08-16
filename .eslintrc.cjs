36c64641f20e0f418c8bbb375881213444fba443
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2022-08-16 04:29:52 +0000

    chore: init repo

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* global module */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
